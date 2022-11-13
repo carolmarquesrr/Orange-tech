d1e8532fd098dd7cdd211b14477f3edc274c75da
Caroline Marques <68516755+carolmarquesrr@users.noreply.github.com>
2022-11-13 00:04:34 +0000

    Exercicio JS da DIO
    Calculo de porcentagem em um produto.

---
+++ ex3.js
@@ -0,0 +1,24 @@
+
+/*algoritmo para calcular e adequar preço pela forma de pagamento
+1 - debito tem 10% desc
+2 -dinheiro pix, 15% desc
+3 - 2x, preço normal sem juros
+4- mais parcelas, 10% juros
+
+
+MEU PRIMEIRO EXERCIO SEM ERRO NENHUM! 
+*/
+
+const precoEtiqueta = 100;
+const formadePagamento = 4 ;
+
+if ( formadePagamento === 1) {
+    console.log(precoEtiqueta - (precoEtiqueta*0.1) );
+
+} else if (formadePagamento === 2) {
+    console.log(precoEtiqueta - (precoEtiqueta*0.15) );
+}else if (formadePagamento ===3) {
+    console.log(precoEtiqueta);
+} else {
+    console.log (precoEtiqueta + (precoEtiqueta*0.1));
+}
